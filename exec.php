86fc41aac8d0f896c4182ac822c1dbd055c25932
drkameleon <yaniszaf@gmail.com>
2021-03-30 07:46:27 +0200

    updated website

--- exec.php
+++ exec.php
@@ -1,42 +1,30 @@
 <?php
 header('Access-Control-Allow-Origin: https://arturo-lang.io');
-//error_reporting(~0);
-//ini_set('display_errors', 1);
+
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 $code = $_POST['c'];
-
-$temp_file = tempnam(sys_get_temp_dir(), 'art');
+if (trim($_POST['i'])!="") {
+	$temp_file = "/tmp/art_".$_POST['i'];
+}
+else {
+	$temp_file = tempnam(sys_get_temp_dir(), 'art_');
+}
 file_put_contents($temp_file, $code."\n");
 
-// //$temp = tmpfile();
-// //fwrite($temp, $code);
-// //fseek($temp, 0);
-// //echo fread($temp, 1024);
+$i = exec ("echo \"\033[0;37m$(/var/www/arturo-lang.io/arturo $temp_file 2>&1)\" | aha --no-header --black",$output);
 
-// //$fname = "tmp".rand(1,10000).".art";
-// //$i = file_put_contents("/var/www/$fname", $code);
-$i = exec ("/var/www/arturo-lang.io/arturo \"$temp_file\" 2>&1",$output);
-
-// //print_r($i);
-// //print_r($output);
+$txt = "";
 foreach ($output as $outp)
 {
-	echo trim($outp)."<br>";
+	$txt .= str_replace("\t","&nbsp;&nbsp;&nbsp;&nbsp;",$outp). "<br>";
 }
-	/*
-// 	$oo = str_replace("[0m","",$outp);
-// 	$oo = str_replace("File","\n| File",$oo);
-// 	$oo = str_replace("[37m","",$oo);
-// 	$oo = str_replace("[32m","",$oo);
-// 	$oo = str_replace("[31m","",$oo);
 
-// 	echo trim($oo)."\n";*/
-// }
+$final = array(
+	"text" => $txt,
+	"code" => str_replace("art_","",str_replace("/tmp/", "", $temp_file))
+);
 
-
-// fclose($temp); // this removes the file
-
-
+echo json_encode($final);
 
 ?>
