961a8c819e65ada6048731710b27864c96e5992b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-23 17:47:39 -0300

    add semantic arguments in get_download_url function

--- installer/install.sh
+++ installer/install.sh
@@ -201,13 +201,13 @@ install_prerequisites() {
 
 get_download_url() { 
     downloadUrl=$( 
-        curl -s "$API_URL"                  \
-          | grep "browser_download_url"     \
-          | grep "$currentOS"               \
-          | grep $VERSION                   \
-          | head -1                         \
-          | cut -d : -f 2,3                 \
-          | tr -d \" 
+        curl -s "$API_URL"                      \
+          | grep "browser_download_url"         \
+          | grep "$currentOS"                   \
+          | grep $VERSION                       \
+          | head --lines 1                      \
+          | cut --delimiter ":" --fields 2,3    \
+          | tr --delete \" 
     )
 }
 
